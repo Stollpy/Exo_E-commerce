b6448425f9e51d4d1c59ef4bce107fc751228788
Florian Darras <darrasflorian42@gmail.com>
2020-12-08 10:03:36 +0100

    File example config

---
+++ config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+// On stocke les informations de connexion PDO dans des constantes
+const DB_HOST = 'hostname';
+const DB_NAME = 'database';
+const DB_USER = 'user';
+const DB_PASSWORD = 'password';
+
+
+
+// Role utilisateur
+
+const ROLE_USER = 'user';
+const ROLE_ADMIN = 'admin';
